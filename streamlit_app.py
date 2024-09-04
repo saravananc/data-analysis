ff4b00cedb6efabb80256871bdac1b9e8ee34fa2
Saravanan Chandrasekaran <csaravananit@gmail.com>
2024-09-04 08:33:21 +0000

    import csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-
+import pandas as pd
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+df= pd.read_csv("./sample-data-line.csv")
+st.line_chart(df)
